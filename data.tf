feca97a192e1acecfdb0338de71a074abc8109c5
Owen Hayes <owen.v.hayes@gmail.com>
2021-09-15 13:56:09 -0700

    Fixed depends on issue

--- data.tf
+++ data.tf
@@ -2,5 +2,5 @@ data "digitalocean_kubernetes_versions" "do_k8s_versions" {}
 
 data "digitalocean_kubernetes_cluster" "k8s" {
   name = var.do_k8s_name
-  depends_on = [digitalocean_kubernetes_cluster.k8s, digitalocean_kubernetes_node_pool.k8s_nodes, digitalocean_kubernetes_cluster.k8s.id, digitalocean_project_resources.do_project_resources]
+  depends_on = [digitalocean_kubernetes_cluster.k8s, digitalocean_kubernetes_node_pool.k8s_nodes, digitalocean_project_resources.do_project_resources]
 }
